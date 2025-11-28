e37cd89ab27ff907b1172944eb15f237bb588e78
jxm1699@student.bham.ac.uk <jxm1699@student.bham.ac.uk>
2025-11-28 16:02:05 +0000

    Made a start on BuildCommand. Need to finish.

--- src/org/uob/a2/parser/Parser.java
+++ src/org/uob/a2/parser/Parser.java
@@ -7,5 +7,23 @@ import java.util.Arrays;
 
 
 public class Parser {
+
+
+
+    public Parser(){}
+    
+    // Return parsed Command object from string user input
+    public Command parse(String command){
+        command = command.trim().toLowerCase(); //remove extra whitespace around string, turn to lowercase
+        String[] splitCmd = command.split("\\s+"); // Split into array of words by whitespace (accounts for two or more whitespaces if entered accidentally)
+        List<String> words = Arrays.asList(splitCmd); // Turns array into list object
+
+        if 
+
+        
+        
+        
+    }
+    
    
 }
